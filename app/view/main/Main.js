8be8def99bf95726ae5ff7597909f4fb79a8176a
landrzejewski <landrzejewski.poczta@gmail.com>
2019-11-08 11:45:13 +0100

    Security

--- app/view/main/Main.js
+++ app/view/main/Main.js
@@ -2,7 +2,7 @@ Ext.define("Tasks.view.main.Main", {
   extend: "Ext.panel.Panel",
   xtype: "app-main",
   requires: [
-    "Tasks.service.L10nService" // wymusza kilejność ładowania
+    "Tasks.service.L10nService" // wymusza kolejność ładowania
   ],
   controller: "main",
   title: "Tasks manager"
